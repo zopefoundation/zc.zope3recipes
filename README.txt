7ad216e9c5f12b80075ec5108acb4469eec8af82
Jim Fulton <jim@zope.com>
2007-02-28 18:54:44 +0000

    Updated release info.
    git-svn-id: file:///home/mg/src/zope-git-migration/zope-mirror/zc.zope3recipes/trunk@72914 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ README.txt
@@ -0,0 +1,14 @@
+**************************************
+Zope3 Application and Instance Recipes
+**************************************
+
+Somewhat experimental recipes for creating Zope 3 instances with
+distinguishing features:
+
+- Don't use a skeletin
+
+- Seprates application and instance definition
+
+- Don't support package-includes
+
+.. contents::
--- setup.py
+++ setup.py
@@ -1,16 +1,34 @@
+import os
 from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+long_description = (
+    read('README.txt')
+    + '\n' +
+    'Detailed Documentation\n'
+    '**********************\n'
+    + '\n' +
+    read('zc', 'zope3recipes', 'README.txt')
+    + '\n' +
+    'Download\n'
+    '**********************\n'
+    )
+
+open('long-description.txt', 'w').write(long_description)
 
 name = "zc.zope3recipes"
 setup(
     name = name,
-    version = "0.2",
+    version = "0.3",
     author = "Jim Fulton",
     author_email = "jim@zope.com",
     description = "ZC Buildout recipe for defining Zope 3 applications",
-    #long_description = open('README.txt').read(),
     license = "ZPL 2.1",
     keywords = "zope3 buildout",
     url='http://svn.zope.org/'+name,
+    long_description=long_description,
 
     packages = find_packages('.'),
     include_package_data = True,
@@ -28,4 +46,11 @@ setup(
     extras_require = dict(
         tests = ['zdaemon', 'zc.recipe.filestorage'],
         ),
+    classifiers = [
+       'Framework :: Buildout',
+       'Intended Audience :: Developers',
+       'License :: OSI Approved :: Zope Public License',
+       'Topic :: Software Development :: Build Tools',
+       'Topic :: Software Development :: Libraries :: Python Modules',
+       ],
     )
