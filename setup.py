1e1c7eaf9db06bea0d0b3db86e1647e3a5c0df4c
Philipp von Weitershausen <philipp@weitershausen.de>
2007-07-13 10:49:37 +0000

    bump version number for future release
    git-svn-id: file:///home/mg/src/zope-git-migration/zope-mirror/zc.zope3recipes/trunk@77837 62d5b8a3-27da-0310-9561-8e5933582275

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ long_description = (
 name = "zc.zope3recipes"
 setup(
     name = name,
-    version = "0.5.2",
+    version = "0.5.3",
     author = "Jim Fulton",
     author_email = "jim@zope.com",
     description = "ZC Buildout recipe for defining Zope 3 applications",
