17a6b0ce5f943e3384a456c008ee6b3ca120c96c
Stephan Richter <stephan.richter@gmail.com>
2007-11-04 00:20:39 +0000

    Get ready for release.
    git-svn-id: file:///home/mg/src/zope-git-migration/zope-mirror/zc.zope3recipes/trunk@81463 62d5b8a3-27da-0310-9561-8e5933582275

--- setup.py
+++ setup.py
@@ -4,32 +4,27 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-long_description = (
-    read('README.txt')
-    + '\n' +
-    'Detailed Documentation\n'
-    '**********************\n'
-    + '\n' +
-    read('zc', 'zope3recipes', 'README.txt')
-    + '\n' +
-    'Download\n'
-    '**********************\n'
-    )
-
-#open('long-description.txt', 'w').write(long_description)
-
 name = "zc.zope3recipes"
 setup(
     name = name,
-    version = "0.6b1",
+    version = "0.6.0",
     author = "Jim Fulton",
     author_email = "jim@zope.com",
     description = "ZC Buildout recipe for defining Zope 3 applications",
     license = "ZPL 2.1",
     keywords = "zope3 buildout",
-    url='http://svn.zope.org/'+name,
-    long_description=long_description,
-
+    url='http://cheeseshop.python.org/pypi/'+name,
+    long_description = (
+        read('README.txt')
+        + '\n' +
+        'Detailed Documentation\n'
+        '**********************\n'
+        + '\n' +
+        read('zc', 'zope3recipes', 'README.txt')
+        + '\n' +
+        'Download\n'
+        '**********************\n'
+        ),
     packages = find_packages('.'),
     include_package_data = True,
     #package_dir = {'':'src'},
@@ -47,11 +42,13 @@ setup(
     extras_require = dict(
         tests = ['zdaemon', 'zc.recipe.filestorage'],
         ),
-    classifiers = [
-       'Framework :: Buildout',
-       'Intended Audience :: Developers',
-       'License :: OSI Approved :: Zope Public License',
-       'Topic :: Software Development :: Build Tools',
-       'Topic :: Software Development :: Libraries :: Python Modules',
-       ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Buildout",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Zope Public License",
+        "Topic :: Software Development :: Build Tools",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
     )
+ 
