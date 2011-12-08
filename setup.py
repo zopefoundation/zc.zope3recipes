9da79ba51f08659f27523cf7e8ce9f9ef7fa99f4
Andrew Sung <asung@zope.com>
2011-12-08 23:01:48 +0000

    tag release 0.14.1
    git-svn-id: file:///home/mg/src/zope-git-migration/zope-mirror/zc.zope3recipes/tags/0.14.1@123642 62d5b8a3-27da-0310-9561-8e5933582275

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(*rnames):
 name = "zc.zope3recipes"
 setup(
     name = name,
-    version = '0',
+    version = '0.14.1',
     author = "Jim Fulton",
     author_email = "jim@zope.com",
     description = "ZC Buildout recipe for defining Zope 3 applications",
